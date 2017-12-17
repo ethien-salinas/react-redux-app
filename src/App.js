a8e5e350068303c9b4a1ad1461e9a0c27c677724
Ethien Salinas <ethien.salinas@gmail.com>
2017-12-16 22:57:17 -0600

    [CHG] Time extension

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Timer from './components/Timer.js'
+import Timer from './components/Timer'
 
 class App extends Component {
   render() {
