b4cfade369e216fa6a11c7acec018a82bb4d10bf
Ethien Salinas <ethien.salinas@gmail.com>
2017-12-21 07:20:08 -0600

    [ADD] TodoApp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Timer from './Timer'
+import Timer from './Timer';
+import TodoApp from './TodoApp';
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Timer/>
+        <TodoApp/>
       </div>
     );
   }
