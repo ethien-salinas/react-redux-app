bfe36dcf1f84633d5aa70b7b8bce4e1fa07c9d80
Ethien Salinas <ethien.salinas@gmail.com>
2017-12-25 18:41:48 -0600

    [CHG] format with editorconfig

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,9 +16,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Timer/>
-        <TodoApp/>
-        <MarkdownEditor/>
+        <Timer />
+        <TodoApp />
+        <MarkdownEditor />
       </div>
     );
   }
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,31 +3,31 @@ import React, { Component } from 'react';
 class Timer extends Component {
 
   constructor(props) {
-      super(props);
-      this.state = { seconds: 0 };
-    }
-  
-    tick() {
-      this.setState(prevState => ({
-        seconds: prevState.seconds + 1
-      }));
-    }
-  
-    componentDidMount() {
-      this.interval = setInterval(() => this.tick(), 1000);
-    }
+    super(props);
+    this.state = { seconds: 0 };
+  }
 
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
-    
-    render() {
-        return (
-            <div>
-            Seconds: {this.state.seconds}
-            </div>
-        );
-    }
+  tick() {
+    this.setState(prevState => ({
+      seconds: prevState.seconds + 1
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div>
+        Seconds: {this.state.seconds}
+      </div>
+    );
+  }
 }
 
 export default Timer;
