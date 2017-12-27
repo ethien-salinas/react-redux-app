98f1160d8219050e95880dc872dae7d47b190773
Ethien Salinas <ethien.salinas@gmail.com>
2017-12-26 23:12:59 -0600

    [ADD] use bulma columns to divide components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,22 +4,42 @@ import './App.css';
 import Timer from './Timer';
 import TodoApp from './TodoApp';
 import MarkdownEditor from './MarkdownEditor';
+import 'bulma/css/bulma.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Timer />
-        <TodoApp />
-        <MarkdownEditor />
-      </div>
+      <section class="section">
+        <section class="section">
+          <div class="container">
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+              </header>
+              <p className="App-intro">
+                This starter has been modified using some examples found in
+              </p>
+              <a class="button is-medium" href='https://reactjs.org' target='_blank' rel="noopener noreferrer">
+                https://reactjs.org
+              </a>
+            </div>
+          </div>
+        </section>
+        <section class="section">
+          <div class="columns">
+            <div class="column">
+              <Timer />
+            </div>
+            <div class="column">
+              <TodoApp />
+            </div>
+            <div class="column">
+              <MarkdownEditor />
+            </div>
+          </div>
+        </section>
+      </section>
     );
   }
 }
