69fa7679f3b0c20459fd13c494f7a3ccb3d7bdf6
Ethien Salinas <ethien.salinas@gmail.com>
2018-01-04 07:36:09 -0600

    [ADD] TodoList presentational component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Todo from './Todo'
 
 class TodoList extends Component {
   render() {
+    const { todos, onTodoClick } = this.props
     return (
       <ul>
-        {this.props.items.map(item => (
-          <li key={item.id}>{item.text}</li>
+        {todos.map(todo => (
+          <Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo.id)} />
         ))}
       </ul>
-    );
+    )
   }
+}
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      completed: PropTypes.bool.isRequired,
+      text: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired,
+  onTodoClick: PropTypes.func.isRequired
 }
 
 export default TodoList;
