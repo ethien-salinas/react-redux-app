1d2af2dcdf449e46897e3200472f6507f22f8c1e
Ethien Salinas <ethien.salinas@gmail.com>
2018-01-13 07:41:23 -0600

    [ADD] use bulma media object to show used technologies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,14 +12,22 @@ class App extends Component {
       <section className="section">
         <div className="columns">
           <div className="column">
-
             <section className="hero is-medium is-dark is-bold">
               <div className="hero-body">
                 <div className="container">
                   <h1 className="title">Welcome to React</h1>
                   <h2 className="subtitle">This starter has been modified using some examples found in:</h2>
-                  <a className="button" href='https://reactjs.org' target='_blank' rel="noopener noreferrer">
-                    https://reactjs.org</a>
+                  <div class="media">
+                    <div class="media-left">
+                      <figure class="image is-48x48">
+                        <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" alt="React image" />
+                      </figure>
+                    </div>
+                    <div class="media-content">
+                      <p class="title is-4">React</p>
+                      <p class="subtitle is-6">https://reactjs.org</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </section>
