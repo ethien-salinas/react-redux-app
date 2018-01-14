6f8bdfe5c1da7ccf4ebd6104c0665054990f1916
Ethien Salinas <ethien.salinas@gmail.com>
2018-01-14 08:53:04 -0600

    [UPDATE] test folder location to take advantage of preconfigured environment of create-react-app

--- src/__test__/sum.test.js
+++ src/__test__/sum.test.js
@@ -1,8 +1,8 @@
-test('one plus two is not zero', () => {
+test('one pllus two is not zero', () => {
   expect(1 + 2).not.toBe(0)
 });
 
-describe('two plus two is four', () => {
+describe('twwo plus two is four', () => {
   it('should be four', () => {
     expect(2 + 2).toBe(4);
   });
