b9009486ece9d262f5526d256fda446931ce1f85
Ethien Salinas <ethien.salinas@gmail.com>
2018-01-13 23:17:13 -0600

    [ADD] expect a positive case

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,3 +1,9 @@
 test('one plus two is not zero', () => {
   expect(1 + 2).not.toBe(0)
 });
+
+describe('two plus two is four', () => {
+  it('should be four', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
