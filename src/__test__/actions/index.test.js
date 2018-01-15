ef57ee6f6706f5e3fd50cbba3638a11da90c65b0
Ethien Salinas <ethien.salinas@gmail.com>
2018-01-15 00:35:23 -0600

    [ADD] test to try ADD_TODO action twice

--- src/__test__/actions/index.test.js
+++ src/__test__/actions/index.test.js
@@ -1,16 +1,25 @@
 import * as actions from '../../actions/'
 
 describe('actions', () => {
+
   it('should create an action to add a todo', () => {
-
     const text = 'Finish docs'
-
     const expectedAction = {
-     id: 0,
+      id: 0,
       type: 'ADD_TODO',
       text
     }
-
     expect(actions.addTodo(text)).toEqual(expectedAction)
   })
+
+  it('should create anther action to add a todo', () => {
+    const text = 'Use Jest with Redux'
+    const expectedAction = {
+      id: 1,
+      type: 'ADD_TODO',
+      text
+    }
+    expect(actions.addTodo(text)).toEqual(expectedAction)
+  })
+
 })
