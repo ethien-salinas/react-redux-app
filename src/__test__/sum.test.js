105aba81cf904f80ea5147d60ac3249057f3ef1d
Ethien Salinas <ethien.salinas@gmail.com>
2018-01-24 06:31:07 -0600

    [DELETE] unneeded test file with a proof of concept

--- src/__test__/sum.test.js
+++
@@ -1,9 +0,0 @@
-test('one pllus two is not zero', () => {
-  expect(1 + 2).not.toBe(0)
-});
-
-describe('twwo plus two is four', () => {
-  it('should be four', () => {
-    expect(2 + 2).toBe(4);
-  });
-});
